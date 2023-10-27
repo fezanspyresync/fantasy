d69928acfb482f99e26c21da4e9a7764b5c4d5ca
FaizanHussainhsp <faizanhussainhsp@gmail.com>
2023-10-26 21:25:16 -0700

    fro

--- android/app/src/main/java/com/fantasy/MainApplication.java
+++ android/app/src/main/java/com/fantasy/MainApplication.java
@@ -1,5 +1,4 @@
 package com.fantasy;
-import com.dooboolab.audiorecorderplayer.RNAudioRecorderPlayerPackage;
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
